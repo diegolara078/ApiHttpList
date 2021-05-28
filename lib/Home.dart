94a78cddadc0e05922344175ed63817d1536ff2a
Diego Lara <diegolara078@gmail.com>
2021-05-28 15:38:20 -0300

    Flutter-Crud

--- lib/Home.dart
+++ lib/Home.dart
@@ -13,9 +13,6 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Consumo de serviço avançado"),
-      ),
       body: FutureBuilder<List<Post>>(
         future: recuperarPostagens(http.Client()),
         builder: (context, snapshot) {
@@ -40,15 +37,45 @@ class PostList extends StatelessWidget {
   PostList({Key? key, required this.post}) : super(key: key);
 
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: post.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          contentPadding: const EdgeInsets.all(8.0),
-          title: Text(post[index].title),
-          subtitle: Text(post[index].body),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de serviço avançado"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                ElevatedButton(
+                  child: Text("Salvar"),
+                  onPressed: createPost,
+                ),
+                ElevatedButton(
+                  child: Text("Atualizar"),
+                  onPressed: updatePut,
+                ),
+                ElevatedButton(
+                  child: Text("Remover"),
+                  onPressed: delete,
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: post.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    contentPadding: const EdgeInsets.all(8.0),
+                    title: Text(post[index].title),
+                    subtitle: Text(post[index].body),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
